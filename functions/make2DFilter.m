418e96acc95fdec4f8c1c9facc8f8b34a66c9e8e
macoskey <macoskey@umich.edu>
2017-10-14 14:35:34 -0400

    some changes to 2D filter

--- functions/make2DFilter.m
+++ functions/make2DFilter.m
@@ -1,5 +1,5 @@
-% function filt2D = make2DFilter(inputFilter,f)
-% Makes 2D filter from 1D filter for filtering 2D signals/images
+function filt2D = make2DFilter(inputFilter,f)
+%% Makes 2D filter from 1D filter for filtering 2D signals/images
 % J. Macoskey
 
 l = length(inputFilter);
@@ -7,23 +7,30 @@ if l > 1000
     rate = round(l/1000);
     inputFilter = decimate(inputFilter,rate);
     fd = decimate(f,rate);
+    dcheck = 1;
+    l = length(inputFilter);
+else
+    dcheck = 0;
 end
 
-%%
-fmax = max(fd);
-maximum = max(inputFilter(:));
-
-x = linspace(-pi,pi,length(inputFilter));
-y = linspace(-pi,pi,length(inputFilter));
+shiftFilter = fftshift(inputFilter);
+x = linspace(-l/2,l/2,length(inputFilter));
+y = linspace(-l/2,l/2,length(inputFilter));
 [xx,yy] = meshgrid(x,y);
 F2 = zeros(size(xx));
 for i = 1:length(xx)
     for j = 1:length(yy)
-        r(i,j) = round(sqrt(xx(i,j).^2+yy(i,j).^2)./sqrt(2*pi^2)*1000);
-        F2(i,j) = inputFilter(r(i,j));
+        r(i,j) = round(sqrt(xx(i,j).^2+yy(i,j).^2))+1;
+        F2(i,j) = shiftFilter(r(i,j));
     end
 end
-imagesc(fd,fd,F2), colormap gray
-axis square
 
-% end
+% This part needs to be fixed:
+if dcheck
+    % interpolate
+    filt2D = F2;
+else
+    filt2D = F2;
+end
+
+end
