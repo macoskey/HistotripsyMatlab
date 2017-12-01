0e19e7fbb5264e889dd6ba9166d738e13bcc132f
macoskey <macoskey@umich.edu>
2017-12-01 17:15:05 -0500

    placeholder for kernel function

---
+++ functions/kernel.m
@@ -0,0 +1,13 @@
+function k = kernel(u,v,opt)
+% This function makes kernel matrices for machine learning applications
+% such as KSVM, KRR, and KPCA, among others. Beta support includes
+% Gaussian, polynomial, and .... other Kernels
+% J. Macoskey
+% 12.1.17
+%
+% Inputs:
+%
+% Outputs:
+
+
+end
