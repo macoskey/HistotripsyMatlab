278f24a504e4a0cc90f9f64d69cb5745af4f11cf
macoskey <macoskey@umich.edu>
2018-03-05 12:16:01 -0500

    fixed lightify

--- functions/lightify.m
+++ functions/lightify.m
@@ -12,9 +12,10 @@ function lightify(fignum,colors)
 %
 
 
-COLORORDER = [0.0000    0.4470    0.7410;
-              0.8500    0.3250    0.0980;
-              0.9290    0.6940    0.1250;
+COLORORDER = [0.8000    0.0000    0.0000;
+              0.0000    0.8000    0.0000;
+              0.0000    0.4471    0.7412;
+              0.1000    0.1000    0.1000;
               0.4940    0.1840    0.5560;
               0.4660    0.6740    0.1880;
               0.3010    0.7450    0.9330;
@@ -24,7 +25,7 @@ figure(fignum)
 set(fignum,'color',[1 1 1])
 set(gca,'Color',[1 1 1])
 l = get(gca,'Legend');
-set(l,'Location','best')
+% set(l,'Location','best')
 set(l,'Color','w')
 set(l,'TextColor',[0 0 0]);
 
@@ -39,10 +40,10 @@ set(gca,'YColor','k')
 set(gca,'ZColor','k')
 set(gca,'GridColor','k')
 
-if(colors)
-    hline = findobj(gcf,'type','line');
-    for n = 1:length(hline)
-        set(hline(n),'LineWidth',2.0)
+hline = findobj(gcf,'type','line');
+for n = 1:length(hline)
+    set(hline(n),'LineWidth',2.0)
+    if(colors)
         try
             set(hline(n),'Color',COLORORDER(mod(n,size(COLORORDER,1)),:));
         catch
@@ -50,4 +51,5 @@ if(colors)
         end
     end
 end
+
 end
